5be2f7e57a92e85182781dda5497eb661dd1ca5e
Kauã Ortolani Lusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2023-05-02 21:13:28 -0300

    Logica de envio de emails concluido

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Home/>
       <AboutMe/>
       <Projects/>
-      <Skills/>
+      {/* <Skills/> */}
       <Contato/>
       <Footer/>
     </div>
--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Contato.module.scss';
 import { HiLocationMarker } from 'react-icons/hi';
 import { MdEmail } from 'react-icons/md';
 import { RiWhatsappFill } from 'react-icons/ri';
+import emailjs from '@emailjs/browser';
 
 const Contato = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // console.log(name)
+  // console.log(email)
+  // console.log(subject)
+  // console.log(mensagem)
+
+  function sedEmail(e) {
+    e.preventDefault();
+    if (name === '' || email === '' || subject === '' || message === '') {
+      setError(true);
+    } else {
+      const templateParams = {
+        name: name,
+        subject: subject,
+        message: message,
+        email: email,
+      };
+      setError(false);
+      emailjs
+        .send(
+          'service_7w8trzb',
+          'template_fze75s6',
+          templateParams,
+          'RogZHWFmgc7vGw9e2',
+        )
+        .then(
+          (response) => {
+            console.log(response.text);
+            console.log(response.status);
+          },
+          (error) => {
+            console.log(error.text);
+          },
+        );
+        setName('')
+        setEmail('')
+        setSubject('')
+        setMessage('')
+    }
+  }
+
   return (
     <section className={styles.contatoContainer}>
       <h1>Contate-me</h1>
@@ -36,12 +85,40 @@ const Contato = () => {
           </div>
         </div>
 
-        <form className={styles.contact}>
-          <input type="text" placeholder='Nome' className={styles.nome}/>
-          <input type="email" placeholder='Email' className={styles.email}/>
-          <input type="text" placeholder='Objetivo' className={styles.subject}/>
-          <textarea style={{resize: 'none'}} name="mensagem" cols="30" rows="10" placeholder='Mensagem' className={styles.mensagem}></textarea>
+        <form onSubmit={sedEmail} className={styles.contact}>
+          <input
+            type="text"
+            placeholder="Nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className={styles.nome}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.email}
+          />
+          <input
+            type="text"
+            placeholder="Objetivo"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            className={styles.subject}
+          />
+          <textarea
+            style={{ resize: 'none' }}
+            name="mensagem"
+            cols="30"
+            rows="10"
+            placeholder="Mensagem"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className={styles.mensagem}
+          ></textarea>
           <button>Enviar mensagem</button>
+          {error && <p>Preencha todos os campos</p>}
         </form>
       </div>
     </section>
